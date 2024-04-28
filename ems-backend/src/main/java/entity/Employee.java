35e770a95dd1525455707c5b252ccfd02ed55060
evgxnx <birki192@gmail.com>
2024-04-28 07:26:15 +0500

    db connection

---
+++ ems-backend/src/main/java/entity/Employee.java
@@ -0,0 +1,26 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "last_name")
+    private String last_name;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
